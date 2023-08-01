983368710df2ab54d073bbfe30c57c9ced997f51
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-08-01 18:31:27 +0530

    splashScreen form cretaed

---
+++ src/lk/pawana/controller/SplashScreenFormController.java
@@ -0,0 +1,10 @@
+package lk.pawana.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class SplashScreenFormController {
+    public Rectangle loadRec1;
+    public Rectangle loadRec2;
+    public Label lblLoading;
+}
